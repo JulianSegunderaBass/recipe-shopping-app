58abd3a8351cd3fe7fba2c784b1bf0ee34c51168
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-11-08 11:00:20 +0800

    Basic layout setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  // Only store Recipe objects in array
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://www.unileverfoodsolutions.com.ph/dam/global-ufs/mcos/SEA/calcmenu/recipes/PH-recipes/chicken-&-other-poultry-dishes/adobo/main-header.jpg')
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+// Typescript shortcut for class properties and constructor
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
