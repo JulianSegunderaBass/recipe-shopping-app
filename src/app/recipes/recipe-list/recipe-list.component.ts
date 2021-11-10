249fada91818ab38b0c17c826dddf91381ca31e9
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2021-11-10 12:29:24 +0800

    Section 6 - Components and Databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipes.model';
 
 @Component({
@@ -9,8 +9,14 @@ import { Recipe } from '../recipes.model';
 export class RecipeListComponent implements OnInit {
   // Only store Recipe objects in array
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'https://www.unileverfoodsolutions.com.ph/dam/global-ufs/mcos/SEA/calcmenu/recipes/PH-recipes/chicken-&-other-poultry-dishes/adobo/main-header.jpg')
+    new Recipe('Adobong Manok', 'Recipe for Adobong Manok', 'https://www.unileverfoodsolutions.com.ph/dam/global-ufs/mcos/SEA/calcmenu/recipes/PH-recipes/chicken-&-other-poultry-dishes/adobo/main-header.jpg'),
+    new Recipe('Pancit Canton', 'Recipe for Pancit Canton', 'https://www.foxyfolksy.com/wp-content/uploads/2019/02/pancit-canton-640.jpg'),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
   constructor() { }
 
